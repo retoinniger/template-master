9ee4afebc9566d7556b8bf1895df9aa28946ccbd
Reto Inniger <reto.inniger@gmx.ch>
2018-12-08 20:04:42 +0100

    Template start

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,7 @@
+class IndexController < ApplicationController
+  def show
+    @page_title = "Homepage"
+  end
+  
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  root to: 'index#index'
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
