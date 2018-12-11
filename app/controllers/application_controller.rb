e413191c985b160aff85d9a903e8f2502627df68
Reto Inniger <reto.inniger@gmx.ch>
2018-12-11 21:02:33 +0100

    first steps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  protect_from_forgery with: :exception
+    
   protected
 
   def configure_permitted_parameters
@@ -10,4 +11,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+  
 end
