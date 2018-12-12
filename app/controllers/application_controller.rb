a4a348e8672ee3730f251601415dcd6460eb0caa
Reto Inniger <reto.inniger@gmx.ch>
2018-12-12 19:41:30 +0100

    Login with user or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     
   protected
-
+  
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,29 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => {email: true, login: false}
   
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }       
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+  
+  attr_accessor :login
+
+  def login
+    @login || self.username || self.email
+  end
+  
+  # function to handle user's login via email or username
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
   
 end
