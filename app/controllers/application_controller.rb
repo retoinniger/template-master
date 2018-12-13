1496abf08e044561c7b792f41bab6db87d8ca88a
Reto Inniger <reto.inniger@gmx.ch>
2018-12-13 21:58:09 +0100

    Add login with user or email worked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,25 @@
 class ApplicationController < ActionController::Base
   
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  
   protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #check_authorization if: :check_authorization?
     
   protected
   
   def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
-   devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :remember_me])
    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
   end
   
+  private
+  
+  def check_authorization?
+    !devise_controller?
+  end
+
+  def authenticate_user?
+    !devise_controller?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,70 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => {email: true, login: false}
-  
-  validates :username, presence: :true, uniqueness: { case_sensitive: false }
-  
-  validate :validate_username
-
-  def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
-  end
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
   
   attr_accessor :login
-
-  def login
-    @login || self.username || self.email
-  end
   
-  # function to handle user's login via email or username
-  def self.find_for_database_authentication warden_conditions
+  validates :username, presence: true
+  validates :username, uniqueness: {case_sensitive: false}
+  
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
-  end
-  
-  protected
-  # Attempt to find a user by it's email. If a record is found, send new
-  # password instructions to it. If not user is found, returns a new user
-  # with an email not found error.
-  def self.send_reset_password_instructions attributes = {}
-    recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-    recoverable.send_reset_password_instructions if recoverable.persisted?
-    recoverable
-  end
-
-  def self.find_recoverable_or_initialize_with_errors required_attributes, attributes, error = :invalid
-    (case_insensitive_keys || []).each {|k| attributes[k].try(:downcase!)}
-
-    attributes = attributes.slice(*required_attributes)
-    attributes.delete_if {|_key, value| value.blank?}
-
-    if attributes.keys.size == required_attributes.size
-      if attributes.key?(:login)
-        login = attributes.delete(:login)
-        record = find_record(login)
-      else
-        record = where(attributes).first
-      end
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions.to_hash).first
     end
-
-    unless record
-      record = new
-
-      required_attributes.each do |key|
-        value = attributes[key]
-        record.send("#{key}=", value)
-        record.errors.add(key, value.present? ? error : :blank)
-      end
-    end
-    record
   end
-
-  def self.find_record login
-    where(["username = :value OR email = :value", {value: login}]).first
-  end
-  
 end
