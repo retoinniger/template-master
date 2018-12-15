94a4aac910ac6cb8413d8f499aad4600b3fce101
Reto Inniger <reto.inniger@gmx.ch>
2018-12-15 21:31:34 +0100

    first test not running

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Showing the home page' do
   it 'displays a welcome message' do
-    visit root_path
-    expect(page).to have_title 'Please Log IN'
+    
+    expect(page).to have_title 'Homepage'
     expect(page).to have_headline 'Hello'
   end
 
