8dd31d35d01aa10eab202b7b59a09402816749c6
Reto Inniger <reto.inniger@gmx.ch>
2018-12-15 17:28:48 +0100

    fix routs

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   root to: 'index#index'
   
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: {confirmations: 'user/confirmations',
+                                   passwords:     'user/passwords',
+                                   registrations: 'user/registrations',
+                                   sessions:      'user/sessions',
+                                   unlocks:       'user/unlocks'}
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Showing the home page' do
   it 'displays a welcome message' do
     visit root_path
-    expect(page).to have_title 'Homeage'
+    expect(page).to have_title 'Please Log IN'
     expect(page).to have_headline 'Hello'
   end
 
